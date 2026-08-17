4fa50056d6fca1a292ed12c592e10301164beaf4
RA <ra@zerotech.ru>
2026-08-17 08:47:51 +0300

    - Практическая работа 6   - попытка поднять защищенный кластер

--- unit_5/scripts/certs.sh
+++ unit_5/scripts/certs.sh
@@ -3,12 +3,13 @@ set -euo pipefail
 
 CA_PASS="kafka123"
 
-CA_DIR="./ca"
-CA_FILE="${CA_DIR}/ca.cnf"
+MOUNT_DIR="./mount_dir"
+TMP_DIR="./tmp_dir"
+CA_FILE="${TMP_DIR}/ca.cnf"
 
 # Очищаем и готовим директорию
-rm -rf ${CA_DIR}
-mkdir -p ${CA_DIR}
+rm -rf ${TMP_DIR}
+mkdir -p ${TMP_DIR} ${MOUNT_DIR}
 
 cat > ${CA_FILE} << EOF
 [ policy_match ]
@@ -45,10 +46,10 @@ EOF
 
 
 # Создадим корневой сертификат (Root CA)
-openssl req -new -nodes -x509 -days 365 -newkey rsa:2048 -keyout "${CA_DIR}/ca.key" -out "${CA_DIR}/ca.crt" -config ${CA_FILE}
+openssl req -new -nodes -x509 -days 365 -newkey rsa:2048 -keyout "${TMP_DIR}/ca.key" -out "${TMP_DIR}/ca.crt" -config ${CA_FILE}
 
 # Создадим файл для хранения сертификата безопасности
-cat "${CA_DIR}/ca.crt" "${CA_DIR}/ca.key" > "${CA_DIR}/ca.pem"
+cat "${TMP_DIR}/ca.crt" "${TMP_DIR}/ca.key" > "${TMP_DIR}/ca.pem"
 
 #################
 
@@ -56,10 +57,11 @@ cat "${CA_DIR}/ca.crt" "${CA_DIR}/ca.key" > "${CA_DIR}/ca.pem"
 ## Создаем сертификаты
 create_cert() {
   local NAME=$1
-  mkdir -p "${CA_DIR}/${NAME}/creds"
+  local DIR_CREDS="${TMP_DIR}/${NAME}/creds"
+  mkdir -p ${DIR_CREDS}
 
 # Создадим файл конфигурации
-  cat > "${CA_DIR}/${NAME}/creds/kafka.cnf" << EOF
+  cat > "${DIR_CREDS}/kafka.cnf" << EOF
 [req]
 prompt = no
 distinguished_name = dn
@@ -98,38 +100,38 @@ EOF
 
   openssl req -new \
       -newkey rsa:2048 \
-      -keyout "${CA_DIR}/${NAME}/creds/kafka.key" \
-      -out "${CA_DIR}/${NAME}/creds/kafka.csr" \
-      -config "${CA_DIR}/${NAME}/creds/kafka.cnf" \
+      -keyout "${DIR_CREDS}/kafka.key" \
+      -out "${DIR_CREDS}/kafka.csr" \
+      -config "${DIR_CREDS}/kafka.cnf" \
       -nodes
 
   # Создадим сертификат, подписанный CA
 
   openssl x509 -req \
       -days 3650 \
-      -in "${CA_DIR}/${NAME}/creds/kafka.csr" \
-      -CA "${CA_DIR}/ca.crt" \
-      -CAkey "${CA_DIR}/ca.key" \
+      -in "${DIR_CREDS}/kafka.csr" \
+      -CA "${TMP_DIR}/ca.crt" \
+      -CAkey "${TMP_DIR}/ca.key" \
       -CAcreateserial \
-      -out "${CA_DIR}/${NAME}/creds/kafka.crt" \
-      -extfile "${CA_DIR}/${NAME}/creds/kafka.cnf" \
+      -out "${DIR_CREDS}/kafka.crt" \
+      -extfile "${DIR_CREDS}/kafka.cnf" \
       -extensions v3_req
 
   # Создадим PKCS12-хранилище
   openssl pkcs12 -export \
-      -in "${CA_DIR}/${NAME}/creds/kafka.crt" \
-      -inkey "${CA_DIR}/${NAME}/creds/kafka.key" \
+      -in "${DIR_CREDS}/kafka.crt" \
+      -inkey "${DIR_CREDS}/kafka.key" \
       -chain \
-      -CAfile "${CA_DIR}/ca.pem" \
+      -CAfile "${TMP_DIR}/ca.pem" \
       -name ${NAME} \
-      -out "${CA_DIR}/${NAME}/creds/kafka.p12" \
+      -out "${DIR_CREDS}/kafka.p12" \
       -password pass:${CA_PASS}
 
   # Создадим keystore для Kafka
   keytool -importkeystore \
       -deststorepass ${CA_PASS} \
-      -destkeystore "${CA_DIR}/${NAME}/creds/keystore.pkcs12" \
-      -srckeystore "${CA_DIR}/${NAME}/creds/kafka.p12" \
+      -destkeystore "${DIR_CREDS}/keystore.pkcs12" \
+      -srckeystore "${DIR_CREDS}/kafka.p12" \
       -deststoretype PKCS12  \
       -srcstoretype PKCS12 \
       -noprompt \
@@ -137,28 +139,54 @@ EOF
 
   # Создадим truststore для Kafka
   keytool -import \
-      -file "${CA_DIR}/ca.crt" \
+      -file "${TMP_DIR}/ca.crt" \
       -alias ca \
-      -keystore "${CA_DIR}/${NAME}/creds/truststore.jks" \
+      -keystore "${DIR_CREDS}/truststore.jks" \
       -storepass ${CA_PASS} \
       -noprompt
 
   # Сохраним пароли
-  echo ${CA_PASS} > "${CA_DIR}/${NAME}/creds/sslkey_creds"
-  echo ${CA_PASS} > "${CA_DIR}/${NAME}/creds/keystore_creds"
-  echo ${CA_PASS} > "${CA_DIR}/${NAME}/creds/truststore_creds"
+  echo ${CA_PASS} > "${DIR_CREDS}/sslkey_creds"
+  echo ${CA_PASS} > "${DIR_CREDS}/keystore_creds"
+  echo ${CA_PASS} > "${DIR_CREDS}/truststore_creds"
 
 }
+
+# admin
+create_cert "admin"
+cat > "${TMP_DIR}/admin/creds/admin-client.properties" << EOF
+security.protocol=SSL
+ssl.truststore.location=/etc/kafka/secrets/admin/truststore.jks
+ssl.truststore.password=${CA_PASS}
+ssl.truststore.type=JKS
+ssl.keystore.location=/etc/kafka/secrets/admin/keystore.pkcs12
+ssl.keystore.password=${CA_PASS}
+ssl.keystore.type=PKCS12
+ssl.key.password=${CA_PASS}
+ssl.endpoint.identification.algorithm=https
+EOF
+
 
 # контроллеры:
 for i in 1; do
   create_cert "kafka-c-${i}"
+
+  mkdir -p "${TMP_DIR}/kafka-c-${i}/creds/admin"
+  cp -r "${TMP_DIR}/admin/creds/" "${TMP_DIR}/kafka-c-${i}/creds/admin/"
 done
+
 
 # брокеры:
 for i in 1; do
   create_cert "kafka-b-${i}"
+
+  mkdir -p "${TMP_DIR}/kafka-b-${i}/creds/admin"
+  cp -r "${TMP_DIR}/admin/creds/" "${TMP_DIR}/kafka-b-${i}/creds/admin/"
 done
 
 # kafka-ui
 create_cert "kafka-ui"
+
+
+cp -r ${TMP_DIR}/ ${MOUNT_DIR}/
+rm -rf ${TMP_DIR}/
